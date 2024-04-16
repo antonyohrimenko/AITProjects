6adeef0e8f6ae5b33122ff58729eb42f53b4ef13
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2024-04-16 14:50:16 +0300

    uptaded 3 line

--- src/lesson16/classwork/Main01.java
+++ src/lesson16/classwork/Main01.java
@@ -6,5 +6,8 @@ public class Main01 {
     public static void main(String[] args) {
         System.out.println("Тест изменений и проверка кеш кода (1ый коммит)");
         System.out.println("Добавил еще одну строку для добавления в коммит");
+        System.out.println("Добавили 2 строку без коммита");
+        System.out.println("Добавил 3 строку");
+
     }
 }
