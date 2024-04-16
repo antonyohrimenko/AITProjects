a0cdde6f4c9fd906dfa863ced7725e282e4d383b
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2024-04-16 14:25:39 +0300

    Added one more line

--- src/lesson16/classwork/Main01.java
+++ src/lesson16/classwork/Main01.java
@@ -2,6 +2,9 @@ package lesson16.classwork;
 
 // теория работы с Git
 
-
 public class Main01 {
+    public static void main(String[] args) {
+        System.out.println("Тест изменений и проверка кеш кода (1ый коммит)");
+        System.out.println("Добавил еще одну строку для добавления в коммит");
+    }
 }
