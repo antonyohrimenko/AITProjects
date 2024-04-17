0ad73dcc65ee32e554b9aba659f8b5ac9867ef36
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2024-04-18 01:01:53 +0300

    Night Update 18 apr.

--- src/lesson17/classwork/Main01.java
+++ src/lesson17/classwork/Main01.java
@@ -4,6 +4,7 @@ public class Main01 {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("pull / push");
     }
 
     // методы пишем тут
