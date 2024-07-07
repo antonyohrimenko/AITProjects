392ec667ac29a9338c17582b54078659f3ede155
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2024-07-07 20:19:44 +0200

    Add NG 34 homework

---
+++ src/NGlesson35/classwork/Main.java
@@ -0,0 +1,6 @@
+package NGlesson35.classwork;
+
+public class Main {
+
+    // тренировались работать с гитом и репозиториями
+}
---
+++ src/NGlesson36/classwork/Main.java
@@ -0,0 +1,8 @@
+package NGlesson36.classwork;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Делаем проектную работу в парах
+    }
+}
